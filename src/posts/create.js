e532723a7930d6b4dd3843d5570a46d5abdebaaa
MichaelHilton <michael.hilton@gmail.com>
2025-10-30 14:55:09 -0400

    Starter code for backend
    added button and fixed translators
    
    fix eslint issues
    
    fix eslint issues

--- src/posts/create.js
+++ src/posts/create.js
@@ -10,6 +10,7 @@ const groups = require('../groups');
 const privileges = require('../privileges');
 const activitypub = require('../activitypub');
 const utils = require('../utils');
+const translate = require('../translate');
 
 module.exports = function (Posts) {
 	Posts.create = async function (data) {
@@ -18,6 +19,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
+		const [isEnglish, translatedContent] = await translate.translate(data);
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
@@ -28,7 +30,7 @@ module.exports = function (Posts) {
 		}
 
 		const pid = data.pid || await db.incrObjectField('global', 'nextPid');
-		let postData = { pid, uid, tid, content, sourceContent, timestamp };
+		let postData = { pid, uid, tid, content, sourceContent, timestamp, isEnglish,translatedContent };
 
 		if (data.toPid) {
 			postData.toPid = data.toPid;
--- vendor/nodebb-theme-harmony-2.1.15/templates/partials/topic/post.tpl
+++ vendor/nodebb-theme-harmony-2.1.15/templates/partials/topic/post.tpl
@@ -82,6 +82,14 @@
 
 		<div class="content text-break" component="post/content" itemprop="text">
 			{posts.content}
+			{{{if !posts.isEnglish }}}
+		        <div class="sensitive-content-message">
+		        <a class="btn btn-sm btn-primary view-translated-btn">Click here to view the translated message.</a>
+		        </div>
+		        <div class="translated-content" style="display:none;">
+		        {posts.translatedContent}
+		        </div>
+	        {{{end}}}
 		</div>
 
 		<div component="post/footer" class="post-footer border-bottom pb-2">
